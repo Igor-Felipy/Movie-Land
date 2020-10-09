6538ce5e8f492dda9c37afa1d25b1893efd88c14
Igor-Felipy <igor.felipy123@gmail.com>
2020-10-09 18:36:29 -0300

    DB created

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__,template_folder='templates')
 app.config.from_object('config')
-
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 from app.controlers import controller
 app.register_blueprint(controller)
--- app/controlers/admin.py
+++ app/controlers/admin.py
@@ -6,3 +6,10 @@ from flask import render_template
 def admin():
     return render_template("admin_login.html")
 
+@controller.route('/new_admin')
+def new_admin():
+    return render_template("new_admin.html")
+
+@controller.route('/new_movie')
+def new_movie():
+    return render_template('new_movie.html')
