8ad872dbfbd71243aed6637d8679f7b3db7af0d1
Igor-Felipy <igor.felipy123@gmail.com>
2020-10-09 20:28:20 -0300

    doing the back end

--- app/controlers/admin.py
+++ app/controlers/admin.py
@@ -1,15 +1,48 @@
 from . import controller
-from flask import render_template
+from app import db
+from flask import render_template, request, url_for, redirect
+from app.models.forms import Adm_form, New_adm_form, New_video_form
+from app.models.tables import Adm, Video
 
-
-@controller.route('/admin')
+@controller.route('/admin', methods=['GET','POST'])
 def admin():
-    return render_template("admin_login.html")
+    form = Adm_form()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            user = Adm.query.filter_by(username=form.user.data).first()
+            if user and user.password == form.password.data:
+                pass #login methods
+            else:
+                #add a flash message
+                return redirect(url_for(admin))
+    else:
+        return render_template("admin_login.html",form=form)
 
-@controller.route('/new_admin')
+@controller.route('/new_admin',methods=['GET','POST'])
 def new_admin():
-    return render_template("new_admin.html")
+    form = New_adm_form()
+    if request.method == 'Post':
+        if form.validate_on_submit():
+            NewAdmData = Adm(form.user.data, form.password.data, form.power.data)
+            db.session.add(NewAdmData)
+            db.session.commit()
+            return redirect(url_for('index')) #add a flash message
+        else:
+            pass #add a flash message
+    else:
+        return render_template("new_admin.html", form=form)
 
-@controller.route('/new_movie')
+@controller.route('/new_movie', methods=['GET','POST'])
 def new_movie():
-    return render_template('new_movie.html')
+    form = New_video_form()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            NewVideoData = Video(form.title.data,form.genre.data,form.serie.data,form.link.data,form.image.data)
+            db.session.add(NewVideoData)
+            db.session.commit()
+            #add a flash message
+            return redirect(url_for('new_movie'))
+        else:
+            pass#add a flash message
+    else:
+        return render_template('new_movie.html'form=form)
--- app/models/forms.py
+++ app/models/forms.py
@@ -1,17 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
+from wtforms import StringField, TextAreaField, IntegerField
 from wtforms.validators import DataRequired
 
-class Adm(FlaskForm):
+class Adm_form(FlaskForm):
     username = StringField('user', validators=[DataRequired()])
     password = StringField('password', validators=[DataRequired()])
 
-class Video(FlaskForm):
+class New_adm_form(FlaskForm):
+    username = StringField('user', validators=[DataRequired()])
+    password = StringField('password', validators=[DataRequired()])
+    power = IntegerField('power', validators=[DataRequired()])
+
+class New_video_form(FlaskForm):
     title = StringField('title', validators=[DataRequired()])
     genre = StringField('genre', validators=[DataRequired()])
     serie_title = StringField('serie', validators=[DataRequired()])
     link = StringField('link', validators=[DataRequired()])
     image = StringField('Image', validators=[DataRequired()])
 
-class Coment(FlaskForm):
+class New_coment_form(FlaskForm):
     coment = TextAreaField('coment', validators=[DataRequired()])
