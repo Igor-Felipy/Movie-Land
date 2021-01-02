45d72cdefb42ded5ddaefcb22748ae05cc072350
Igor-Felipy <igor.felipy123@gmail.com>
2021-01-02 16:54:49 -0300

    exigindo autenticacao em todas as rotas

--- app/controlers/admin.py
+++ app/controlers/admin.py
@@ -31,51 +31,60 @@ def logout():
         logout_user()
         return redirect(url_for("index"))
     else:
-        return redirect(url_for('admin'))
+        return redirect(url_for('controller.admin'))
 
 
 @controller.route('/new_admin/',methods=['GET','POST'])
 def new_admin():
-    form = New_adm_form()
-    if request.method == 'Post':
-        if form.validate_on_submit():
-            NewAdmData = Adm(form.user.data, form.password.data, form.power.data)
-            db.session.add(NewAdmData)
-            db.session.commit()
-            return redirect(url_for('index')) #add a flash message
+    if current_user.is_authenticated:
+        form = New_adm_form()
+        if request.method == 'Post':
+            if form.validate_on_submit():
+                NewAdmData = Adm(form.user.data, form.password.data, form.power.data)
+                db.session.add(NewAdmData)
+                db.session.commit()
+                return redirect(url_for('index')) #add a flash message
+            else:
+                pass #add a flash message
         else:
-            pass #add a flash message
+            return render_template("new_admin.html", form=form)
     else:
-        return render_template("new_admin.html", form=form)
-
+        return redirect(url_for('controller.admin'))
 
 @controller.route('/new_movie/', methods=['GET','POST'])
 def new_movie():
-    form = New_video_form()
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            NewVideoData = Video(form.title.data,form.genre.data,form.link.data,form.image.data)
-            db.session.add(NewVideoData)
-            db.session.commit()
-            #add a flash message
-            return redirect(url_for('new_movie'))
+    if current_user.is_authenticated:
+        form = New_video_form()
+        if request.method == 'POST':
+            if form.validate_on_submit():
+                NewVideoData = Video(form.title.data,form.genre.data,form.link.data,form.image.data)
+                db.session.add(NewVideoData)
+                db.session.commit()
+                #add a flash message
+                return redirect(url_for('new_movie'))
+            else:
+                pass#add a flash message
         else:
-            pass#add a flash message
+            return render_template('new_movie.html',form=form)
     else:
-        return render_template('new_movie.html',form=form)
+        return redirect(url_for('controller.admin'))
+
 
 @controller.route('/edit/<int:id>/', methods=['GET','POST'])
 def edit(id):
-    form = New_video_form()
-    video = Video.query.filter_by(id=id).first()
-    if request.method == "POST":
-        video.title = form.title.data
-        video.link = form.link.data
-        video.image = form.image.data
-        video.genre = form.genre.data
-        video.description = form.description.data
-        db.session.commit()
-        #add a flash message
-        return redirect(url_for('index'))
-    else:
-        return render_template('edit.html',form=form, video=video)
+    if current_user.is_authenticated:
+        form = New_video_form()
+        video = Video.query.filter_by(id=id).first()
+        if request.method == "POST":
+            video.title = form.title.data
+            video.link = form.link.data
+            video.image = form.image.data
+            video.genre = form.genre.data
+            video.description = form.description.data
+            db.session.commit()
+            #add a flash message
+            return redirect(url_for('index'))
+        else:
+            return render_template('edit.html',form=form, video=video)
+    else: 
+        return redirect(url_for('controller.admin'))
